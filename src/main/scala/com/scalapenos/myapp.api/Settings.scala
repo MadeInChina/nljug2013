d40eddb2189e1e73b7a0ac2dd1fb94b217e2ef83
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2013-11-05 20:47:30 +0100

    Added settings

---
+++ src/main/scala/com/scalapenos/myapp.api/Settings.scala
@@ -0,0 +1,25 @@
+package com.scalapenos.myapp.api
+
+import java.util.concurrent.TimeUnit
+
+import scala.concurrent.duration._
+
+import akka.actor._
+import com.typesafe.config.Config
+
+
+class Settings(config: Config, extendedSystem: ExtendedActorSystem) extends Extension {
+
+  object Http {
+    val Port = config.getInt("my-app.http.port")
+    val Host = config.getString("my-app.http.host")
+  }
+
+  val askTimeout = FiniteDuration(config.getMilliseconds("my-app.ask-timeout"), TimeUnit.MILLISECONDS)
+}
+
+
+object Settings extends ExtensionId[Settings] with ExtensionIdProvider {
+  override def lookup = Settings
+  override def createExtension(system: ExtendedActorSystem) = new Settings(system.settings.config, system)
+}
